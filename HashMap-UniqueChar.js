8496eae27e58f1366f62d9e7d28353112d38fa7e
Abhinav-Mathew-Kurian <abhinavmathewkurina@gmail.com>
2025-07-14 20:28:38 +0530

    first Non Repeat String Char in a string

---
+++ HashMap-UniqueChar.js
@@ -0,0 +1,36 @@
+// First Unique Character in a String
+// ❓ Question:
+// Given a string s, return the index of the first non-repeating character.
+// If there is none, return -1.
+
+// Input:  s = "leetcode"
+// Output: 0   // 'l' is the first non-repeating
+
+// Input:  s = "loveleetcode"
+// Output: 2   // 'v' is the first non-repeating
+
+// Input:  s = "aabb"
+// Output: -1  // all characters repeat
+
+function firstNonRepeatStringChar(inp){
+    const charCounts=new Map();
+    let position=-1;
+     let count=0;
+      for (let i = 0; i < inp.length; i++) {
+        const char = inp[i];
+        charCounts.set(char, (charCounts.get(char) || 0) + 1);
+    }
+    for (let i=0;i<inp.length;i++){
+        const char = inp[i];
+        if (charCounts.get(char) === 1) {
+            return i; 
+        }
+    }
+    return -1;
+}
+
+const input="leetcode"
+const ans = firstNonRepeatStringChar(input);
+console.log("First non repeating char position is:",ans)
+
+
