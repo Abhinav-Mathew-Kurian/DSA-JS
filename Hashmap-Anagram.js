2127b2ac0a0dff9274b0b8bcc35d782f5724a74e
Abhinav-Mathew-Kurian <abhinavmathewkurina@gmail.com>
2025-07-14 19:49:19 +0530

    Hashmap basic

---
+++ Hashmap-Anagram.js
@@ -0,0 +1,64 @@
+//  Problem 1: Valid Anagram
+
+// Input: s = "anagram", t = "nagaram"
+// Output: true
+
+// Input: s = "rat", t = "car"
+// Output: false
+// ❗ Constraint:
+// Two strings are anagrams if they have the same characters with same frequency.
+function mapAreEqual(map1,map2){
+    if (map1===map2){
+        return true;
+    }
+    else if(map1.size !== map2.size){
+        return false;
+    }
+    else{
+        for(const [key,val] of map1){
+            if (!map2.has(key) || val !== map2.get(key)) {
+            return false;
+        }
+        }
+        return true;
+    }
+}
+
+function anagramChecker(inp1,inp2){
+    const map1 = new Map();
+    const map2 =new Map();
+
+    for(let char of inp1){
+        if(map1.has(char)){
+            map1.set(char,map1.get(char)+1)
+        }else{
+            map1.set(char,1)
+            
+        }
+    }
+     for(let char of inp2){
+        
+        if(map2.has(char)){
+            map2.set(char,map2.get(char)+1)
+        }else{
+            map2.set(char,1)
+            
+        }
+    }
+        console.log("MAP 1:",map1)
+        console.log("MAP 2:",map2)
+
+      let answer= mapAreEqual(map1,map2)
+
+    return answer;
+}
+
+
+
+
+const input1="rat" ;
+const input2="tar";
+
+
+const ans = anagramChecker(input1,input2)
+console.log(ans)
