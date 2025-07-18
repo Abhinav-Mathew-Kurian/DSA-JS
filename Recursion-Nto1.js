ea3191e3b3242ee44b5a9680a0a7e94c9ab8aea5
Abhinav-Mathew-Kurian <abhinavmathewkurian@gmail.com>
2025-07-18 20:35:01 +0530

    Recursion Print N to 1

--- Recursion-Nto1.js
+++ Recursion-Nto1.js
@@ -1,11 +1,9 @@
+// Print Numbers from N to 1 (Descending Order)
 
-
-function nToOne(num){
-    if(num===0) return;
-     console.log(num)
-    nToOne(num-1)
+function nToOne(num) {
+  if (num === 0) return;
+  console.log(num);
+  nToOne(num - 1);
 }
-
-
 
 nToOne(6);
