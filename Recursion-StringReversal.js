f62bcb1826adbcc10af860847e8946a0473854dd
Abhinav-Mathew-Kurian <abhinavmathewkurian@gmail.com>
2025-07-18 22:54:09 +0530

    Recursion sum of digits

---
+++ Recursion-StringReversal.js
@@ -0,0 +1,11 @@
+// recursion of reversal of a string
+
+function reverseStr(str){
+
+    if(str.length<=1) return str;
+    return reverseStr(str.slice(1))+str[0];
+}
+
+
+
+console.log(reverseStr("abhinav")); // Expected output: "olleh"
